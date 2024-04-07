f2bcb63d27b0791accae5f69a174e772158144dd
Vlad Sidorov <mylongmind@gmail.com>
2024-04-07 15:02:49 +0300

    Fix some queries

--- scripts/cn_queries.sql
+++ scripts/cn_queries.sql
@@ -15,4 +15,28 @@ LAG(order_id) OVER (PARTITION BY client_id ORDER BY order_dttm) AS prev_order,
 LEAD(order_id) OVER (PARTITION BY client_id ORDER BY order_dttm) AS next_order
 FROM cn.order WHERE client_id IS NOT NULL;
 
+-- 5. Вывести среднюю стоимость сеанса фильма с id=4 по кинотеатрам
+SELECT *,
+AVG(cost) OVER (PARTITION BY cinema_id) AS avg_cost
+FROM cn.session WHERE film_id = 4;
 
+-- 6. Вывести информацию об оплатах за последний год с нарастающей суммой оплаты
+SELECT payment_id, payment_amount, cn.order.order_dttm,
+SUM(payment_amount) OVER (ORDER BY cn."order".order_dttm)
+FROM cn.payment LEFT OUTER JOIN cn."order" ON cn.payment.order_id = cn."order".order_id
+WHERE order_dttm >= NOW() - INTERVAL '1 year';
+
+-- 7. Вывести все уникальные имя-фамилия среди работников сети
+SELECT DISTINCT first_name, mid_name FROM cn.employee;
+
+-- 8. Вывести только информацию про товары-попкорн
+SELECT * FROM cn.product WHERE LOWER(cn.product.name) LIKE '%попкорн%';
+
+-- 9. Вывести статистику по товарам: товар - сколько раз его покупали
+WITH buy_count AS
+(SELECT product_id, COUNT(order_id) AS buy_count FROM cn.products_in_orders GROUP BY product_id ORDER BY product_id)
+SELECT name, buy_count FROM cn.product LEFT OUTER JOIN buy_count ON cn.product.product_id = buy_count.product_id ORDER BY buy_count DESC ;
+
+-- 10. Вывести кол-во клиентов с бонусной картой
+SELECT (SELECT COUNT(*) FROM cn.client) AS total_count, (SELECT COUNT(*) FROM cn.client LEFT OUTER JOIN cn.loyalty_card ON cn.client.client_id = cn.loyalty_card.client_id
+WHERE loyalty_card.loyalty_card_number IS NOT NULL) AS with_card;
